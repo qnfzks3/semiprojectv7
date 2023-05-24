d23b3153eb35f5352835dd78579d0263025e8faf
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-05-24 17:21:43 +0900

    업로드,다운로드 자료실 - 2. 글쓰기 / 폼/js 파일 작성

---
+++ src/main/resources/static/js/pds.js
@@ -0,0 +1,20 @@
+
+
+
+//새글 쓰기
+const writebtn =document.querySelector("#writebtn");
+
+writebtn?.addEventListener('click',()=>{ //jsp에 있는 인풋의 name에 적힌 이름=> 폼에 적힌 인풋네임  bdfrm.title
+    const pdsfrm=document.forms.pdsfrm;          //왜있는거지?
+
+    if(pdsfrm.title.value ==='') alert('제목을 작성해주세요');
+    else if(pdsfrm.content.value ==='') alert('본문을 작성해주세요!');
+    else if(grecaptcha.getResponse()==='') alert('자동가입방지 체크가 안되어있습니다.');
+    else{
+        bdfrm.method = 'post';            //메서드를 포스트로 해야하며
+        bdfrm.enctype = 'multipart/form-data';  //엔씨타입이 폼데이터로 해놓고  첨부한거 인코딩 한다.
+        bdfrm.submit();
+
+    }
+
+});
