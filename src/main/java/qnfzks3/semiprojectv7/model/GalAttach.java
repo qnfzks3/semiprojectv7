0c8eff98f7ee80abeb57f0819cd525c525fe69f7
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-06-07 12:19:46 +0900

    갤러리 - 갤러리 정의

---
+++ src/main/java/qnfzks3/semiprojectv7/model/GalAttach.java
@@ -0,0 +1,24 @@
+package qnfzks3.semiprojectv7.model;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "galattach")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GalAttach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gano;
+
+    private String fname;
+    private String fsize;
+
+    private Integer gno;
+
+
+}
