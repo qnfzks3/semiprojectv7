1fd49a66b34849ccac1be7ed2458fd7e0972d1ea
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-06-07 12:23:54 +0900

    갤러리 - 첨부시 이미지 파일 여부 체크

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -1,6 +1,26 @@
 // 이미지 첨부 조건 검사
 const checkAttachs = () => {
+    let checkOk = true;
+    const attachs = document.querySelector("#attachs");
 
+    // 이미지 첨부파일이 하나 이상이라면
+    if ('files' in attachs && attachs.files.length > 0) {
+        // 첨부파일들이 이미지인지 검사
+        for (attach of attachs.files) {
+            //console.log(attach.name + ',' + attach.type + ',' + attach.size);
+            // 이미지 파일의 MIME 형식
+            // image/jpg,image/jpeg,image/png,image/gif
+            if (!attach.type.startsWith('image')) {
+                alert('첨부하려는 파일은 반드시 이미지여야 합니다!');
+                checkOk = false;
+            }
+        }
+    } else {
+        alert('하나 이상의 이미지를 선택하세요!!');
+        checkOk = false;
+    }
+
+    return checkOk;
 };
 
 // 새글쓰기
