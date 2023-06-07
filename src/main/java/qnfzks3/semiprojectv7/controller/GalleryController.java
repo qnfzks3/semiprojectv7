547f06dbce2a184d5d986938aafd66b9dc688d40
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-06-07 12:20:02 +0900

    갤러리 - 컨트롤러정의

--- src/main/java/qnfzks3/semiprojectv7/controller/GalleryController.java
+++ src/main/java/qnfzks3/semiprojectv7/controller/GalleryController.java
@@ -1,8 +1,17 @@
 package qnfzks3.semiprojectv7.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import qnfzks3.semiprojectv7.model.Gallery;
+import qnfzks3.semiprojectv7.model.Pds;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/gallery")
@@ -11,6 +20,26 @@ public class GalleryController {
     @GetMapping("/list")
     public String list() {
         return "gallery/list";
+    }
+
+
+    @GetMapping("/write")
+    public String write(Model m) {
+        m.addAttribute("gallery", new Gallery());
+
+        return "gallery/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Gallery gallery, List<MultipartFile> attachs) {
+        String viewPage = "error";
+
+        /*Map<String, Object> pinfo = pdssrv.newPds(pds);
+        if (!attach.isEmpty()) // 첨부파일이 존재한다면
+            pdssrv.newPdsAttach(attach, pinfo);
+        viewPage = "redirect:/pds/list";*/
+
+        return viewPage;
     }
 
 }
