a61a9fc4b8d36182ea5c54b96aaa26db6a1061c2
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-06-08 12:49:03 +0900

    빨간줄 떠있는게 거슬려서 다지움 그냥 무시하셈

--- src/test/java/qnfzks3/semiprojectv7/MemberTest.java
+++ src/test/java/qnfzks3/semiprojectv7/MemberTest.java
@@ -19,39 +19,9 @@ public class MemberTest {
 
 
 
-    @Test
-    @DisplayName("member delete")
-    public void deleteMember()
-    {
-
-        Member m = new Member();
-        m.setMbno(2L);
 
 
 
-        memberRepository.delete(m);
-
-
-    }
-
-    @Test
-    @DisplayName("member login")
-    public void loginMember()
-    {
-
-        Member m = new Member();
-        m.setUserid("abc123");
-        m.setPasswd("abc123");
-
-
-
-        assertNull(memberRepository.countByUseridAndPasswd(m.getUserid(),m.getPasswd()));
-
-
-        m.setUserid("abc123");
-        m.setPasswd("987xyz");
-        assertNull(memberRepository.countByUseridAndPasswd(m.getUserid(),m.getPasswd()));
-    }
 
 
 }
