73fdd52fb14b288693799babbc96d43c3fe189b6
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-06-08 12:48:25 +0900

    빨간줄 떠있는게 거슬려서 다지움 그냥 무시하셈

--- src/test/java/qnfzks3/semiprojectv7/MemberTest.java
+++ src/test/java/qnfzks3/semiprojectv7/MemberTest.java
@@ -17,44 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 @SpringBootTest
 public class MemberTest {
 
-    @Autowired
-    MemberRepository memberRepository;
 
-    @Test
-    @DisplayName("member save")
-    public void saveMember()
-    {
-
-       Member m = new Member(null,"abc123","123456",
-               "1234567","abc123","987xyz","123-456",
-               "서울시 구로구 구로동","더조은 IT 아카데미",
-               "abc123@987xyz.co.kr","123-1234-5678",null);
-
-
-
-       memberRepository.save(m);
-
-
-    }
-
-
-    @Test
-    @DisplayName("member update")
-    public void updateMember()
-    {
-
-        Member m = new Member(null,"abc123","123456",
-                "1234567","abc123","987xyz","123-456",
-                "서울시 구로구 구로동","더조은 IT 아카데미",
-                "abc123@987xyz.co.kr","123-1234-5678",null);
-        //null 이라고 해도 자동으로 값이 채워지도록 dao에 설정 했기 때문에 상관없다.
-
-
-
-        memberRepository.save(m);
-
-
-    }
 
     @Test
     @DisplayName("member delete")
